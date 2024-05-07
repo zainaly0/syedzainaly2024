e5589500bd720e5538b6f6a0f1c71b5feea96cf3
Zaid Aly <zaidk4076@gmail.com>
2024-05-08 00:07:32 +0530

    Portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './Component/Home/Home'
+function App() {
+
+  return (
+    <>
+
+    <main>
+    <Home />
+    </main>
+      
+    </>
+  )
+}
+
+export default App
