d5477280bf4102eede4c1a2aee38058b5b9f647f
Zaid Aly <zaidk4076@gmail.com>
2024-05-09 23:51:36 +0530

    urdu page work

--- src/Component/Urdu/Urdu.jsx
+++ src/Component/Urdu/Urdu.jsx
@@ -18,12 +18,15 @@ const Urdu = () => {
       <div className="urdu__container">
         <div className='name_flag'>
           <img className='flag' src={blackFlag} alt="" />
-          <h1>ملا زین مجاہد</h1>
+          {/* <h1>ملا زین مجاہد</h1> */}
+          <div className="heading">
+          <h1>ISLAM</h1>
+          <h3>Mullah Zain Mujahid</h3>
+          </div>
           <img className='flag' src={whiteFlag} alt="" />
         </div>
-        <h5>free hokr pura krunga is page ko </h5>
-        <h5>busy in study...</h5>
-        <h5>will be complete soon..</h5>
+        <h5>busy in job && study...</h5>
+        <h5>complete will be soon..</h5>
       </div>
 
 
